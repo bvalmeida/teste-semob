622a16a929e12c6d47907e88064df8cbfcf911e7
bvalmeida <bvalmeida94@gmail.com>
2023-01-26 22:12:06 -0300

    refact - refatorando primeira e segunda pagina, corrigindo bugs

--- frontend/second.js
+++ frontend/second.js
@@ -1,25 +1,26 @@
-// var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute']);
 
-// app.config(function($routeProvider) {
-// 	$routeProvider
-// 		.when('/index', {
-// 			templateUrl: 'index.html',
-// 			controller: 'cepController'
-// 		})
-// 		.when('/second', {
-// 			templateUrl: 'second.html',
-// 			controller: 'SecondController'
-// 		})
-// 		.otherwise({
-// 			redirectTo: '/index'
-// 		});
-// });
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/index', {
+			templateUrl: 'index.html',
+			controller: 'cepController'
+		})
+		.when('/second', {
+			templateUrl: 'second.html',
+			controller: 'SecondController'
+		})
+		.otherwise({
+			redirectTo: '/index'
+		});
+});
 
 app.controller('secondController', function($scope, navigationService){
     $scope.goBack = function(){
         navigationService.goToPage('/index.html');
         console.log('teste');
     }
+
 });
 
 app.service('navigationService', function($location){
